c4f1140c34d1cb36f1a8ce3a07a3521b6cd11613
bigben3918 <benjamin199551@gmail.com>
2023-09-28 12:17:45 -0700

    update: env

--- scripts/1-deploy-sample.ts
+++ scripts/1-deploy-sample.ts
@@ -8,15 +8,17 @@ async function main() {
     const indexBasisArtifacts = locklift.factory.getContractArtifacts("IndexBasis");
     const { contract: sample, tx } = await locklift.factory.deployContract({
         contract: "Collection",
-        publicKey: signer.publicKey,
-        initParams: {},
+        publicKey: signer?.publicKey as string,
+        initParams: {
+            nonce_: locklift.utils.getRandomNonce(),
+        },
         constructorParams: {
             codeNft: nftArtifacts.code,
             codeIndex: indexArtifacts.code,
             codeIndexBasis: indexBasisArtifacts.code,
             json: JSON.stringify(collection_content),
         },
-        value: locklift.utils.toNano(5),
+        value: locklift.utils.toNano(4),
     });
 
     console.log(`Collection deployed at: ${sample.address.toString()}`);
